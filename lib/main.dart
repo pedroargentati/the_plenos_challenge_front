2f621b67abec4fd1c255206dab48fbddc974bd33
bruno.ferraz <bhferrazsilva@gmail.com>
2023-10-06 16:46:35 -0300

    fix: Ajuste tela

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,25 @@ class CarScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white, // Define o fundo branco
       appBar: AppBar(
-        backgroundColor: const Color(0xFFbcdbf1),
-        title: const Text('Meu Carro'),
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(width: 8),
+            Text(
+              'My Car',
+              style: TextStyle(
+                  color: Colors.black, fontSize: 15, fontFamily: 'Comfortaa-Light'),
+            ),
+            Icon(
+              Icons.keyboard_arrow_down,
+              color: Colors.black,
+            ),
+          ],
+        ),
         centerTitle: true,
-        titleTextStyle: const TextStyle(color: Colors.black, fontSize: 20),
         actions: const [Icon(Icons.account_circle)],
       ),
       body: ListView(
@@ -35,37 +49,88 @@ class CarScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                ),
-                child: IconButton(
-                  icon: const Icon(Icons.lock_open),
-                  onPressed: () {},
+              InkWell(
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.1),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: const Icon(
+                    Icons.lock_open,
+                    size: 20,
+                    color: Color.fromARGB(255, 0, 2, 118),
+                  ),
                 ),
               ),
               const SizedBox(width: 20),
-              Container(
-                width: 70,
-                height: 70,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                ),
-                child: IconButton(
-                  icon: const Icon(Icons.power_settings_new),
-                  onPressed: () {
-                  },
-                  iconSize: 48,
+              InkWell(
+                child: Container(
+                  width: 70,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.1),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.power_settings_new,
+                        size: 36,
+                        color: Color.fromARGB(255, 0, 2, 118),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Start',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Color.fromARGB(255, 0, 2, 118),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               const SizedBox(width: 20),
-              Container(
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                ),
-                child: IconButton(
-                  icon: const Icon(Icons.lock),
-                  onPressed: () {},
+              InkWell(
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.1),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: const Icon(
+                    Icons.lock,
+                    size: 20,
+                    color: Color.fromARGB(255, 0, 2, 118),
+                  ),
                 ),
               ),
             ],
@@ -77,16 +142,35 @@ class CarScreen extends StatelessWidget {
             ),
             child: Column(
               children: [
-                IconButton(
-                  icon: const Icon(Icons.local_gas_station),
-                  onPressed: () {
+                InkWell(
+                  onTap: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
                           builder: (context) => const ListCarsScreen()),
                     );
                   },
-                  iconSize: 28,
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.1),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: const Icon(
+                      Icons.local_gas_station,
+                      size: 20,
+                      color: Color.fromARGB(255, 0, 2, 118),
+                    ),
+                  ),
                 ),
                 const SizedBox(height: 4),
               ],
@@ -96,25 +180,37 @@ class CarScreen extends StatelessWidget {
         ],
       ),
       bottomNavigationBar: Container(
-        color: const Color(0xFFbcdbf1),
+        color: const Color.fromARGB(255, 255, 255, 255),
         height: 60,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             IconButton(
-              icon: const Icon(Icons.home),
+              icon: const Icon(
+                Icons.home,
+                color: Color.fromARGB(255, 0, 2, 118),
+              ),
               onPressed: () {},
             ),
             IconButton(
-              icon: const Icon(Icons.directions_car),
+              icon: const Icon(
+                Icons.directions_car,
+                color: Color.fromARGB(255, 0, 2, 118),
+              ),
               onPressed: () {},
             ),
             IconButton(
-              icon: const Icon(Icons.help_outline),
+              icon: const Icon(
+                Icons.help_outline,
+                color: Color.fromARGB(255, 0, 2, 118),
+              ),
               onPressed: () {},
             ),
             IconButton(
-              icon: const Icon(Icons.more_horiz),
+              icon: const Icon(
+                Icons.more_horiz,
+                color: Color.fromARGB(255, 0, 2, 118),
+              ),
               onPressed: () {},
             ),
           ],
