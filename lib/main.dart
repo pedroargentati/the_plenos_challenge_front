530307becc662962d629bcaf8c9a4c478f8e03a6
bruno.ferraz <bhferrazsilva@gmail.com>
2023-10-06 20:50:55 -0300

    fix: Ajuste nos ícones e textos

--- lib/main.dart
+++ lib/main.dart
@@ -19,137 +19,37 @@ class CarScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Define o fundo branco
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 255, 255, 255),
-        title: const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(width: 8),
-            Text(
-              'My Car',
-              style: TextStyle(
-                  color: Colors.black, fontSize: 15, fontFamily: 'Comfortaa-Light'),
-            ),
-            Icon(
-              Icons.keyboard_arrow_down,
-              color: Colors.black,
-            ),
-          ],
-        ),
-        centerTitle: true,
-        actions: const [Icon(Icons.account_circle)],
-      ),
-      body: ListView(
-        children: <Widget>[
-          Image.network(
-            'https://images.pexels.com/photos/14438397/pexels-photo-14438397.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-          ),
-          const SizedBox(height: 20),
-          Row(
+        backgroundColor: Colors.white, // Define o fundo branco
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 255, 255, 255),
+          title: const Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              InkWell(
-                child: Container(
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.1),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: const Icon(
-                    Icons.lock_open,
-                    size: 20,
-                    color: Color.fromARGB(255, 0, 2, 118),
-                  ),
-                ),
+              SizedBox(width: 8),
+              Text(
+                'My Ford',
+                style: TextStyle(
+                    color: Colors.black, fontSize: 15, fontFamily: 'Comfortaa'),
               ),
-              const SizedBox(width: 20),
-              InkWell(
-                child: Container(
-                  width: 70,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.1),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.power_settings_new,
-                        size: 36,
-                        color: Color.fromARGB(255, 0, 2, 118),
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Start',
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Color.fromARGB(255, 0, 2, 118),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(width: 20),
-              InkWell(
-                child: Container(
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.1),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: const Icon(
-                    Icons.lock,
-                    size: 20,
-                    color: Color.fromARGB(255, 0, 2, 118),
-                  ),
-                ),
+              Icon(
+                Icons.keyboard_arrow_down,
+                color: Colors.black,
               ),
             ],
           ),
-          const SizedBox(height: 20),
-          Container(
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
+          centerTitle: true,
+          actions: const [Icon(Icons.account_circle)],
+        ),
+        body: ListView(
+          children: <Widget>[
+            Image.asset(
+              'assets/images/ford.jpeg',
             ),
-            child: Column(
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ListCarsScreen()),
-                    );
-                  },
                   child: Container(
                     width: 50,
                     height: 50,
@@ -166,56 +66,199 @@ class CarScreen extends StatelessWidget {
                       ],
                     ),
                     child: const Icon(
-                      Icons.local_gas_station,
+                      Icons.lock_open,
                       size: 20,
                       color: Color.fromARGB(255, 0, 2, 118),
                     ),
                   ),
                 ),
-                const SizedBox(height: 4),
+                const SizedBox(width: 20),
+                InkWell(
+                  child: Container(
+                    width: 70,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.1),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.power_settings_new,
+                          size: 36,
+                          color: Color.fromARGB(255, 0, 2, 118),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Start',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Color.fromARGB(255, 0, 2, 118),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                InkWell(
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.1),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: const Icon(
+                      Icons.lock,
+                      size: 20,
+                      color: Color.fromARGB(255, 0, 2, 118),
+                    ),
+                  ),
+                ),
               ],
             ),
-          ),
-          const SizedBox(height: 20),
-        ],
-      ),
-      bottomNavigationBar: Container(
-        color: const Color.fromARGB(255, 255, 255, 255),
-        height: 60,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-              icon: const Icon(
-                Icons.home,
-                color: Color.fromARGB(255, 0, 2, 118),
+            const SizedBox(height: 20),
+            Container(
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
               ),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: const Icon(
-                Icons.directions_car,
-                color: Color.fromARGB(255, 0, 2, 118),
+              child: Column(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ListCarsScreen()),
+                      );
+                    },
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.1),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: const Icon(
+                        Icons.local_gas_station,
+                        size: 20,
+                        color: Color.fromARGB(255, 0, 2, 118),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                ],
               ),
-              onPressed: () {},
             ),
-            IconButton(
-              icon: const Icon(
-                Icons.help_outline,
-                color: Color.fromARGB(255, 0, 2, 118),
-              ),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: const Icon(
-                Icons.more_horiz,
-                color: Color.fromARGB(255, 0, 2, 118),
-              ),
-              onPressed: () {},
-            ),
+            const SizedBox(height: 20),
           ],
         ),
-      ),
-    );
+        bottomNavigationBar: Container(
+          color: const Color.fromARGB(255, 255, 255, 255),
+          height: 90,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: const Icon(
+                      Icons.home_outlined,
+                      color: Color.fromARGB(255, 133, 133, 148),
+                    ),
+                    onPressed: () {},
+                  ),
+                  const Text(
+                    'Início',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 133, 133, 148),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: const Icon(
+                      Icons.directions_car_outlined,
+                      color: Color.fromARGB(255, 133, 133, 148),
+                    ),
+                    onPressed: () {},
+                  ),
+                  const Text(
+                    'Veículo',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 133, 133, 148),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: const Icon(
+                      Icons.engineering_outlined,
+                      color: Color.fromARGB(255, 133, 133, 148),
+                    ),
+                    onPressed: () {},
+                  ),
+                  const Text(
+                    'Serviços',
+                    style: TextStyle( 
+                      color: Color.fromARGB(255, 133, 133, 148),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: const Icon(
+                      Icons.pin_drop_outlined,
+                      color: Color.fromARGB(255, 133, 133, 148),
+                    ),
+                    onPressed: () {},
+                  ),
+                  const Text(
+                    'Mapa',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 133, 133, 148),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
