77db1721bc2f407aa64e97e70b381853aa01eab6
bruno.ferraz <bhferrazsilva@gmail.com>
2023-10-07 16:14:23 -0300

    fix: renderização da bolinha

--- lib/map.dart
+++ lib/map.dart
@@ -3,7 +3,14 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  double circleRadius = 4000; // Raio inicial do círculo em metros
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +28,14 @@ class MapScreen extends StatelessWidget {
         options: MapOptions(
           center: LatLng(-23.574165, -46.623128),
           zoom: 9.2,
+          onPositionChanged: (MapPosition position, bool hasGesture) {
+            setState(() {
+              final zoom = position.zoom;
+              // Defina uma lógica para ajustar o raio com base no nível de zoom
+              // Por exemplo, você pode fazer o raio diminuir à medida que o zoom aumenta
+              circleRadius = 2000 / zoom!;
+            });
+          },
         ),
         nonRotatedChildren: [
           RichAttributionWidget(
@@ -42,10 +57,10 @@ class MapScreen extends StatelessWidget {
             circles: [
               CircleMarker(
                 point: LatLng(-23.574165, -46.623128),
-                radius: 1000,
+                radius: circleRadius,
                 useRadiusInMeter: true,
-                color: const Color.fromARGB(255, 0, 2, 118),
-                borderColor: Color.fromARGB(255, 255, 255, 255),
+                color: const Color.fromARGB(128, 0, 2, 118),
+                borderColor: const Color.fromARGB(255, 255, 255, 255),
               ),
             ],
           ),
