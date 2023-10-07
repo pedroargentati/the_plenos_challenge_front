56cf96e91aaeffb3a214a70edd59cf1ea0bf0a3d
bruno.ferraz <bhferrazsilva@gmail.com>
2023-10-07 17:21:15 -0300

    fix: Ajuste no tamanho da bolinha quando abre o mapa

--- lib/map.dart
+++ lib/map.dart
@@ -9,7 +9,13 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  double circleRadius = 4000; // Raio inicial do círculo em metros
+  List<LatLng> coordinates = [
+    LatLng(-23.513498, -46.653354),
+    LatLng(-23.513494, -46.653365),
+    LatLng(-23.509061, -46.652768),
+  ];
+
+  double circleRadius = 1000; 
 
   @override
   Widget build(BuildContext context) {
@@ -24,43 +30,60 @@ class _MapScreenState extends State<MapScreen> {
           ),
         ),
       ),
-      body: FlutterMap(
-        options: MapOptions(
-          center: LatLng(-23.574165, -46.623128),
-          zoom: 9.2,
-          onPositionChanged: (MapPosition position, bool hasGesture) {
-            setState(() {
-              final zoom = position.zoom;
-              circleRadius = 2000 / zoom!;
-            });
-          },
-        ),
-        nonRotatedChildren: [
-          RichAttributionWidget(
-            attributions: [
-              TextSourceAttribution(
-                'OpenStreetMap contributors',
-                onTap: () =>
-                    launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-              ),
-            ],
-          ),
-        ],
+      body: Column(
         children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.example.app',
-          ),
-          CircleLayer(
-            circles: [
-              CircleMarker(
-                point: LatLng(-23.574165, -46.623128),
-                radius: circleRadius,
-                useRadiusInMeter: true,
-                color: const Color.fromARGB(128, 0, 2, 118),
-                borderColor: const Color.fromARGB(255, 255, 255, 255),
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            alignment: Alignment.center,
+            child: const Text(
+              'Postos de Gasolina',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontFamily: 'Comfortaa',
               ),
-            ],
+            ),
+          ),
+          Expanded(
+            child: FlutterMap(
+              options: MapOptions(
+                center: LatLng(-23.574165, -46.623128),
+                zoom: 12,
+                onPositionChanged: (MapPosition position, bool hasGesture) {
+                  setState(() {
+                    final zoom = position.zoom;
+                    circleRadius = 3000 / zoom!;
+                  });
+                },
+              ),
+              nonRotatedChildren: [
+                RichAttributionWidget(
+                  attributions: [
+                    TextSourceAttribution(
+                      'OpenStreetMap contributors',
+                      onTap: () => launchUrl(
+                          Uri.parse('https://openstreetmap.org/copyright')),
+                    ),
+                  ],
+                ),
+              ],
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                ),
+                CircleLayer(
+                  circles: coordinates.map((coordinate) {
+                    return CircleMarker(
+                      point: coordinate,
+                      radius: circleRadius,
+                      useRadiusInMeter: true,
+                      color: const Color.fromARGB(128, 0, 2, 118),
+                      borderColor: const Color.fromARGB(255, 255, 255, 255),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
           ),
         ],
       ),
